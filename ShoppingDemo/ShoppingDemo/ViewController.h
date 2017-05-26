50f14a1242ef2b12562a98e90640297c73229dc2
shahanamh <thanamolsa@gmail.com>
2017-05-26 10:04:46 +0530

    files added

---
+++ ShoppingDemo/ShoppingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShoppingDemo
+//
+//  Created by codemac-02i on 5/26/17.
+//  Copyright © 2017 codemac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ShoppingDemo/ShoppingDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShoppingDemo
+//
+//  Created by codemac-02i on 5/26/17.
+//  Copyright © 2017 codemac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
